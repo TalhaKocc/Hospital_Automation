b74d2d8e67c4a4c6d3a149d7cc881d2975cc53e1
TalhaKocc <talhakocc1834@gmail.com>
2025-11-29 00:50:30 +0300

    Merge branch 'main' of https://github.com/TalhaKocc/Hospital_Automation.git into main

--- hospital-automation/src/main/java/com/hospital/automation/dto/ListAllDoctorDto.java
+++ hospital-automation/src/main/java/com/hospital/automation/dto/ListAllDoctorDto.java
@@ -1,5 +1,22 @@
 package com.hospital.automation.dto;
 
-public class ListAllDoctorDto {
 
+import com.hospital.automation.model.Doctor;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ListAllDoctorDto {
+	private Integer doctorId;
+	private String doctorName;
+	private String doctorSurname;
+	private String doctorDepartment;
+	private String doctorPhoneNumber;
+	private String doctorRoomNumber;
+	private Doctor.Gender doctorGender;
+	private Double doctorSalary;
 } 
