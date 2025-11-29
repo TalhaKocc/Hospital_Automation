f26d2c555131189561e8ec0e9dd79035af06f856
TalhaKocc <talhakocc1834@gmail.com>
2025-11-29 17:34:31 +0300

    repository katmanı tamamlandı

---
+++ hospital-automation/src/main/java/com/hospital/automation/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.hospital.automation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hospital.automation.model.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+}
---
+++ hospital-automation/src/main/java/com/hospital/automation/repository/DoctorRepository.java
@@ -0,0 +1,11 @@
+package com.hospital.automation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hospital.automation.model.Doctor;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+
+}
